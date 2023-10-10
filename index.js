d40404585131ec35e90e1d08892525b42dc328a5
githubuchunemail <githubuchunemail@gmail.com>
2023-10-10 10:54:24 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,62 @@
+// Function declaration
+
+// bigNumberFunc(8, 16)
+
+// function bigNumberFunc(a, b){
+//     if(a < b){
+//         console.log(a);
+//     } else {
+//         console.log(b);
+//     }
+// }
+
+
+// Function expression
+
+
+// const bigNumberFunc = function(a, b, c){
+
+//     if(a < b && a < c){
+//         console.log(a + " kichik son");
+//     } else if(b < a && b < c){
+//         console.log(b + " kichik son");
+//     } else {
+//         console.log(c + " kichik son");
+//     }
+// }
+
+// bigNumberFunc(5, 0, 0)
+
+
+// Arrow function
+
+// const myFunc = (son) => {
+//     if(son > 0){
+//        return son = son - 2
+//     } else {
+//        return --son
+//     }
+// }
+
+// console.log(myFunc(-5));
+
+
+// const myFunc = function(son){
+//     if(son > 0){
+//         return ++son
+//     } else if(son == 0){
+//         return son = 10
+//     } else {
+//         return son = son - 2
+//     }
+// }
+
+// console.log(myFunc(-5));
+
+let random = Math.floor(Math.random() * 100)
+
+text.textContent = random
+
+
+
+
